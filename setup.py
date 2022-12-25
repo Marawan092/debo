dd27f1061e7cafaed7a6404a6fdfe3e7be0c0168
محمد الكنيج <120821139+Marawan092@users.noreply.github.com>
2022-12-26 00:27:43 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ try:
   from config import *
   os.system('pm2 start bot.py --name {} --interpreter python3.7 --interpreter-args -u'.format(BOT_ID))
 except Exception as e:
-  API_ID = 11966986
-  API_HASH = '53e499a620b4d132a8b0adb1015b04b5'
+  API_ID = 793178
+  API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
 
   out ="""
 from pyrogram import enums
-API_ID = 11966986
-API_HASH = '53e499a620b4d132a8b0adb1015b04b5'
+API_ID = 793178
+API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
 """
   def Bot(TOKEN,method,data):
     url = "https://api.telegram.org/bot{}/{}".format(TOKEN,method)
